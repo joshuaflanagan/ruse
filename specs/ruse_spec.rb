0f852225b26ea01b77b0ad3bfc2450d8d4b5e5c9
Joshua Flanagan <joshuaflanagan@gmail.com>
2014-02-03 21:16:14 -0600

    fixup! Create an unconfigured

--- specs/ruse_spec.rb
+++ specs/ruse_spec.rb
@@ -4,6 +4,6 @@ require 'ruse'
 
 describe Ruse do
   it "can create an injector" do
-    Ruse.create_injector.must_be_instance_of Ruse::Injector
+    Ruse.create_injector.must_be_kind_of Ruse::Injector
   end
 end
