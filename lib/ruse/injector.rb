3824cd305934f9bbe06dc85f6b13e6ff6602f443
Joshua Flanagan <joshuaflanagan@gmail.com>
2014-02-03 21:41:43 -0600

    Populate dependencies for a requested instance

--- lib/ruse/injector.rb
+++ lib/ruse/injector.rb
@@ -2,11 +2,31 @@ module Ruse
   class Injector
     def get(identifier)
       type = resolve_type identifier
-      type.new
+      args = resolve_dependencies type
+      type.new *args
     end
 
     def resolve_type(identifier)
-      Object.const_get identifier
+      type_name = classify(identifier)
+      unless Object.const_defined?(type_name)
+        raise UnknownServiceError.new(type_name)
+      end
+      Object.const_get type_name
+    end
+
+    def resolve_dependencies(type)
+      type.instance_method(:initialize).parameters.map{|_, identifier|
+        get identifier
+      }
+    end
+
+    def classify(term)
+      # lifted from active_support gem: lib/active_support/inflector/methods.rb
+      string = term.to_s
+      string = string.sub(/^[a-z\d]*/) { $&.capitalize }
+      string.gsub(/(?:_|(\/))([a-z\d]*)/i) { "#{$1}#{ $2.capitalize}" }.gsub('/', '::')
     end
   end
+
+  class UnknownServiceError < StandardError; end
 end
--- specs/injector_spec.rb
+++ specs/injector_spec.rb
@@ -7,5 +7,38 @@ describe Ruse::Injector do
     injector = Ruse::Injector.new
     injector.get("SomeService").must_be_instance_of(SomeService)
   end
+
+  it "raises UnknownServiceError for an identifier it cannot resolve" do
+    injector = Ruse::Injector.new
+    ->{
+      injector.get("cannot_be_resolved")
+    }.must_raise(Ruse::UnknownServiceError)
+  end
+
+  it "populates dependencies for the instance it retrieves" do
+    injector = Ruse::Injector.new
+    instance = injector.get("ServiceConsumer")
+    instance.must_be_instance_of(ServiceConsumer)
+    instance.service1.must_be_instance_of(SomeService)
+    instance.service2.must_be_instance_of(OtherService)
+  end
+
   class SomeService; end
+  class OtherService; end
+
+  class ServiceConsumer
+    attr_reader :service1, :service2
+    def initialize(some_service, other_service)
+      @service1 = some_service
+      @service2 = other_service
+    end
+  end
+end
+
+# classify will be moved
+describe "classify" do
+  it "converts an underscored_term to PascalCase" do
+    resolver = Ruse::Injector.new
+    resolver.classify("camel_case").must_equal("CamelCase")
+  end
 end
