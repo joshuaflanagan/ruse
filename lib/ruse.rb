357800230da6e52d9331832d92cd7924965dc9da
Joshua Flanagan <joshuaflanagan@gmail.com>
2014-02-26 22:15:55 -0600

    .create_injector can configure injector

--- lib/ruse.rb
+++ lib/ruse.rb
@@ -2,7 +2,9 @@ require "ruse/injector"
 require "ruse/version"
 
 module Ruse
-  def self.create_injector
-    Ruse::Injector.new
+  def self.create_injector(config=nil)
+    Ruse::Injector.new.tap do |i|
+      i.configure(config) if config
+    end
   end
 end
--- specs/ruse_spec.rb
+++ specs/ruse_spec.rb
@@ -6,4 +6,9 @@ describe Ruse do
   it "can create an injector" do
     Ruse.create_injector.must_be_kind_of Ruse::Injector
   end
+
+  it "passes configuration to the injector" do
+    injector = Ruse.create_injector({values: {answer: 42}})
+    injector.get(:answer).must_equal 42
+  end
 end
